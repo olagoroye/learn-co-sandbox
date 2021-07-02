147de81b35ade13d44021285798ca8f08735d168
olusola olagoroye <solaolagoroye@gmail.com>
2021-07-02 13:23:08 +0000

    Automatically backed up by Learn

---
+++ challenge.rb
@@ -0,0 +1,2 @@
+def badge_maker ("names")
+  puts "Hello my name is #{names [0]}"
---
+++ example.rb
@@ -0,0 +1,7 @@
+def my_method (movie)
+  
+  puts "my most favourite movie is #{movie}"
+  
+end
+
+my_method = ( "Titanic", "Dumber", "Hey you" )
